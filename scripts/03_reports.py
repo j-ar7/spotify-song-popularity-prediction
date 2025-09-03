e0abc6be4cd19ed18735a0a9fef33f2a6540c82f
j-ar7 <112698622+j-ar7@users.noreply.github.com>
2025-09-04 01:49:09 +0530

    Update 03_reports.py

--- scripts/03_reports.py
+++ scripts/03_reports.py
@@ -5,9 +5,13 @@ import seaborn as sns
 import os
 from sklearn.model_selection import train_test_split
 
+start = time.perf_counter()
+
+print('loading model and scaler..')
 model = joblib.load('saved_models/random_forest_model.joblib')
 scaler = joblib.load('saved_models/scaler.joblib')
 
+print('loading model_data.csv..')
 df = pd.read_csv('processed_data/model_data.csv')
 
 output_dir = 'figures'
@@ -29,26 +33,27 @@ plt.title('Feature Correlation Matrix', fontsize=18)
 
 output_path = os.path.join(output_dir, 'feature_correlation_heatmap.png')
 plt.savefig(output_path, bbox_inches='tight')
-print(f"Heatmap saved successfully to: {output_path}")
+print(f"heatmap saved successfully to: {output_path}..")
 plt.close()
 
 y = df['popularity']
 X = df.drop('popularity', axis=1)
 _, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+N = 15
 importances = model.feature_importances_
 feature_names = X.columns
 feature_importance_df = pd.DataFrame({'feature': feature_names, 'importance': importances})
-feature_importance_df = feature_importance_df.sort_values('importance', ascending=False).head(15)
+feature_importance_df = feature_importance_df.sort_values('importance', ascending=False).head(N)
 
 plt.figure(figsize=(12, 8))
 sns.barplot(x='importance', y='feature', data=feature_importance_df)
-plt.title('10 most important features for popularity', fontsize=16)
+plt.title(f'{N} most important features for popularity', fontsize=16)
 plt.xlabel('Importance')
 plt.ylabel('Feature')
 
 plt.savefig(os.path.join(output_dir, 'feature_importance.png'), bbox_inches='tight')
-print(f"Feature importance plot saved to '{output_dir}/feature_importance.png'")
+print(f"feature importance plot saved to '{output_dir}/feature_importance.png..'")
 plt.close()
 
 X_test_scaled = scaler.transform(X_test)
@@ -56,7 +61,7 @@ y_pred = model.predict(X_test_scaled)
 
 plt.figure(figsize=(10, 10))
 sns.scatterplot(x=y_test, y=y_pred, alpha=0.3)
-plt.plot([0, 100], [0, 100], color='red', linestyle='--', linewidth=2) # Perfect prediction line
+plt.plot([0, 100], [0, 100], color='red', linestyle='--', linewidth=2)
 plt.title('Actual vs. Predicted Popularity', fontsize=16)
 plt.xlabel('Actual Popularity')
 plt.ylabel('Predicted Popularity')
@@ -64,5 +69,6 @@ plt.xlim(0, 100)
 plt.ylim(0, 100)
 
 plt.savefig(os.path.join(output_dir, 'prediction_scatter_plot.png'), bbox_inches='tight')
-print(f"Prediction scatter plot saved to '{output_dir}/prediction_scatter_plot.png'")
+print(f"prediction scatter plot saved to '{output_dir}/prediction_scatter_plot.png..'")
 plt.close()
+print(f'[fin in {time.perf_counter()-start:.3f} s..]')
